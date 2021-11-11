b8a6e500e99659f49d37fb9ef31078fb75674602
turtlebasket <turtlebasket@gmail.com>
2021-11-11 13:14:45 -0800

    new config format - preliminary multi-smart-chain support

--- main.go
+++ main.go
@@ -23,10 +23,41 @@ func main() {
 
 		config.LoadSingleConfig()
 
-		fmt.Printf("Found payout addresses:")
-		for k, v := range config.SingleConfig["addresses"].(map[interface{}]interface{}) {
+		supportEth := false
+
+		// currently unused
+		supportBnb := false
+		supportAvax := false
+
+		fmt.Println("Found payout addresses:")
+		for k, v := range config.SingleConfig["wallets"].(map[interface{}]interface{}) {
+			switch k {
+			case "eth":
+				supportEth = true
+			case "avax":
+				supportAvax = true
+			}
 			fmt.Printf("\n%s:\t%s", strings.ToUpper(fmt.Sprint(k)), v)
 		}
+
+		fmt.Println("")
+
+		// Smart chain token support
+
+		if supportEth {
+			fmt.Println("Found Ethereum tokens:")
+			for _, v := range config.SingleConfig["preferences"].(map[interface{}]interface{})["ethereum_tokens"].([]interface{}) {
+				fmt.Printf("%s  ", strings.ToUpper(fmt.Sprint(v)))
+			}
+		}
+
+		if supportBnb {
+			fmt.Println("Binance is currently unsupported.")
+		}
+		if supportAvax {
+			fmt.Println("Avalanche is currently unsupported.")
+		}
+
 	} else {
 		// preflight checks for web UI API will go here
 
