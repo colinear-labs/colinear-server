6ce2aaea25755e89de27ac33e25679ba9e763ef6
turtlebasket <turtlebasket@gmail.com>
2021-11-11 16:14:50 -0800

    change config layout

--- main.go
+++ main.go
@@ -19,46 +19,19 @@ func main() {
 	flag.Parse()
 
 	flags.Mode = *mode
-	if *mode == "single" {
+
+	if flags.Mode == "single" {
 
 		config.LoadSingleConfig()
 
-		supportEth := false
-
-		// currently unused
-		supportBnb := false
-		supportAvax := false
-
 		fmt.Println("Found payout addresses:")
 		for k, v := range config.SingleConfig["wallets"].(map[interface{}]interface{}) {
-			switch k {
-			case "eth":
-				supportEth = true
-			case "avax":
-				supportAvax = true
-			}
 			fmt.Printf("\n%s:\t%s", strings.ToUpper(fmt.Sprint(k)), v)
 		}
 
 		fmt.Println("")
 
-		// Smart chain token support
-
-		if supportEth {
-			fmt.Println("Found Ethereum tokens:")
-			for _, v := range config.SingleConfig["preferences"].(map[interface{}]interface{})["ethereum_tokens"].([]interface{}) {
-				fmt.Printf("%s  ", strings.ToUpper(fmt.Sprint(v)))
-			}
-		}
-
-		if supportBnb {
-			fmt.Println("Binance is currently unsupported.")
-		}
-		if supportAvax {
-			fmt.Println("Avalanche is currently unsupported.")
-		}
-
-	} else {
+	} else if flags.Mode == "community" {
 		// preflight checks for web UI API will go here
 
 		panic("Community node is not implemented yet.")
