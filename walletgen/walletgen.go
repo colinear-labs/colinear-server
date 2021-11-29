a5d5aadeac7748b123e721c016572d92be8bac5f
turtlebasket <turtlebasket@gmail.com>
2021-11-29 10:42:51 -0800

    check currencies.ethtokens instead of reimplemented list

--- walletgen/walletgen.go
+++ walletgen/walletgen.go
@@ -4,21 +4,24 @@ import (
 	"fmt"
 	"math/rand"
 	"strings"
+	"xserver/xutil/currencies"
 
 	"github.com/foxnut/go-hdwallet"
 )
 
 var mnemonic, _ = hdwallet.NewMnemonic(12, "en")
-var Master, err = hdwallet.NewKey(
+var Master, _ = hdwallet.NewKey(
 	hdwallet.Mnemonic(mnemonic),
 )
 
 func GenerateNewWallet(currency string) hdwallet.Wallet {
 	var ctype uint32
-	switch strings.ToLower(currency) {
+	curr := strings.ToLower(currency)
+
+	switch curr {
 	case "btc":
 		ctype = hdwallet.BTC
-	case "eth", "dai", "usdc", "usdt", "ust", "ampl":
+	case "eth":
 		ctype = hdwallet.ETH
 	case "bch":
 		ctype = hdwallet.BCH
@@ -26,7 +29,17 @@ func GenerateNewWallet(currency string) hdwallet.Wallet {
 		ctype = hdwallet.LTC
 	case "doge":
 		ctype = hdwallet.DOGE
+	default: // else - unknown
+		for _, x := range currencies.EthTokens {
+			if curr == x {
+				ctype = hdwallet.ETH
+				goto currencyFound
+			}
+		}
+		// can check other currencies down here if necessary
 	}
+
+currencyFound:
 
 	addressIndex := rand.Uint32()
 	wallet, err := Master.GetWallet(hdwallet.CoinType(ctype), hdwallet.AddressIndex(addressIndex))
